e2ecb4e0f7ad29a9b1ee99105f111b250813e0fd
Jonas Hauth <30393216+JonasHauth@users.noreply.github.com>
2022-07-13 16:52:42 +0200

    initial version with crud

---
+++ app.py
@@ -0,0 +1,68 @@
+from flask import Flask, jsonify, request
+from eventRepository import eventRepository
+
+app = Flask(__name__)
+repo = eventRepository()
+
+'''
+Sample Request Body
+{
+    "summary"
+    "start"
+    "end"
+
+}
+
+'''
+
+@app.route('/', methods=['GET'])
+def get_events():
+    todos = repo.get_all()
+    response = jsonify(todos)
+    response.status_code = 200
+    return response
+
+
+@app.route('/<string:event_id>', methods=['GET'])
+def get_event(event_id):
+    event = repo.get_id(event_id)
+    response = jsonify(event)
+    response.status_code = 200
+    return response
+
+
+@app.route('/', methods=['POST'])
+def save_event():
+    event = request.get_json()
+    new_event = repo.save(event)
+    response = jsonify(repo.get_id(new_event))
+    response.status_code = 201
+    return response
+
+@app.route('/', methods=['PUT'])
+def update_event():
+    body = request.get_json()
+    event_id = body["_id"]
+    updated = repo.update(body)
+    if updated >= 1:
+        response = jsonify(repo.get_id(event_id))
+        response.status_code = 200
+    else:
+        response = jsonify({"status_code": 404})
+    return response
+
+
+@app.route('/<string:event_id>', methods=['DELETE'])
+def delete_event(event_id):
+    deleted = repo.delete(event_id)
+    if deleted >= 1:
+        response = jsonify({"status_code": 200})
+    else:
+        response = jsonify({"status_code": 404})
+    return response
+
+
+
+
+if __name__ == '__main__':
+    app.run(port=8080)
---
+++ eventRepository.py
@@ -0,0 +1,41 @@
+from pymongo import MongoClient
+from bson import json_util, ObjectId
+import json
+
+
+class eventRepository:
+
+    def __init__(self):
+        self.client = MongoClient('localhost', 27017)
+        self.db = self.client.workday_db
+        self.events = self.db.event_collection
+
+
+    def get_id(self, event_id):
+        event = self.events.find_one({"_id": ObjectId(event_id)})
+        event = json.loads(json_util.dumps(event))
+        event["_id"] = event["_id"]["$oid"]
+        return event
+
+    def get_all(self):
+        cursor = self.events.find({})
+        events = list(cursor)
+        events = json.loads(json_util.dumps(events))
+        for item in events:
+            item["_id"] = item["_id"]["$oid"]
+        return events
+
+    def save(self, event):
+        persisted_event = self.events.insert_one(event)
+        new_id = json.loads(json_util.dumps(persisted_event.inserted_id))
+        return list(new_id.values())[0]
+
+    def update(self, event):
+        event_id = event["_id"]
+        del event["_id"]
+        result = self.events.update_one(filter={"_id": ObjectId(event_id)}, update={"$set": event})
+        return result.modified_count
+
+    def delete(self, event):
+        result = self.events.delete_one({"_id": ObjectId(event)})
+        return result.deleted_count
