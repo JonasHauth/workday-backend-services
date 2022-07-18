7d064753c13b261f0383dd63e6d4de7c035c7b1d
Jonas Hauth <30393216+JonasHauth@users.noreply.github.com>
2022-07-18 14:02:49 +0200

    add ical serve

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response, send_from_directory
 from eventRepository import eventRepository
+from icalendar import Calendar, Event, vCalAddress
+import json
+
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+import os
+import requests
+import datetime
+from datetime import date
+from pathlib import Path
 
 app = Flask(__name__)
 repo = eventRepository()
@@ -14,15 +26,75 @@ Sample Request Body
 
 '''
 
-@app.route('/', methods=['GET'])
+@app.route('/sync/google', methods=['POST'])
+def sync_google():
+
+    json_credentials = request.get_json()
+
+    print(json_credentials)
+    
+    credentials = Credentials(
+        token=json_credentials['token'],
+        token_uri=json_credentials['token_uri'], 
+        client_id=json_credentials['client_id'],
+        client_secret=json_credentials['client_secret'],
+    )
+
+    try:
+        service = build('calendar', 'v3', credentials=credentials)
+
+        # Call the Calendar API
+        time =  datetime.datetime.utcnow() - datetime.timedelta(weeks=+4)
+        now = time.isoformat() + 'Z' #indicates UTC time
+        print(now)
+        print('Getting the upcoming 10 events')
+
+        events_result = service.events().list(calendarId='primary', timeMin=now,
+                                            maxResults=40, singleEvents=True,
+                                            orderBy='startTime').execute()
+        events = events_result.get('items', [])
+
+        # Delete existing events from google calendar
+        myquery = { "from_google": 1 }
+        count = repo.delete_many(myquery)
+        print(count)
+
+
+        # Save synced events from google calendar
+        for event in events:
+            summary = event['summary']
+            start = event['start'].get('dateTime')
+            end = event['end'].get('dateTime')
+            email = event['creator'].get('email')
+                
+            event_dict = {
+                "title": summary,
+                "start": start,
+                "end": end,
+                "email": email,
+                "from_google": 1
+            }
+
+            repo.save(event_dict)
+
+    except HttpError as error:
+        print('An error occurred: %s' % error)
+
+    response = jsonify(events)
+    response.status_code = 200
+    return response
+    
+
+@app.route('/calendar', methods=['GET'])
 def get_events():
-    todos = repo.get_all()
-    response = jsonify(todos)
+    events = repo.get_all()
+    response = jsonify(events)
     response.status_code = 200
     return response
 
 
-@app.route('/<string:event_id>', methods=['GET'])
+
+@app.route('/calendar/<string:event_id>', methods=['GET'])
 def get_event(event_id):
     event = repo.get_id(event_id)
     response = jsonify(event)
@@ -30,7 +102,7 @@ def get_event(event_id):
     return response
 
 
-@app.route('/', methods=['POST'])
+@app.route('/calendar', methods=['POST'])
 def save_event():
     event = request.get_json()
     new_event = repo.save(event)
@@ -38,7 +110,7 @@ def save_event():
     response.status_code = 201
     return response
 
-@app.route('/', methods=['PUT'])
+@app.route('/calendar', methods=['PUT'])
 def update_event():
     body = request.get_json()
     event_id = body["_id"]
@@ -51,7 +123,7 @@ def update_event():
     return response
 
 
-@app.route('/<string:event_id>', methods=['DELETE'])
+@app.route('/calendar/<string:event_id>', methods=['DELETE'])
 def delete_event(event_id):
     deleted = repo.delete(event_id)
     if deleted >= 1:
@@ -60,8 +132,44 @@ def delete_event(event_id):
         response = jsonify({"status_code": 404})
     return response
 
+@app.route('/calendar/ics')
+def calendar_ics():
 
+    #  Get the calendar data
+    cal = Calendar()
 
+    events = repo.get_all()
+    print(events)
+
+    for dbEvent in events:
+        
+
+        try: 
+
+            event = Event()
+            event.add('summary', dbEvent['title'])
+            dtstart = datetime.datetime.strptime( str(dbEvent['start']), "%Y-%m-%dT%H:%M:%S%z")
+            event.add('dtstart', dtstart)
+            dtend = datetime.datetime.strptime(str(dbEvent['end']), "%Y-%m-%dT%H:%M:%S%z")
+            event.add('dtend', dtend)
+
+            organizer = vCalAddress(dbEvent['email'])
+
+            event['organizer'] = organizer
+
+            cal.add_component(event)
+        
+        except:
+            print('invalid event')
+
+    directory = str(Path(__file__).parent) + "\\"
+    print("ics file will be generated at ", directory)
+    f = open(os.path.join(directory, 'calendar.ics'), 'wb')
+    f.write(cal.to_ical())
+    f.close()
+
+    path = os.path.join(directory)
+    return send_from_directory(directory=path, path="calendar.ics", as_attachment=True)
 
 if __name__ == '__main__':
     app.run(port=8080)
--- eventRepository.py
+++ eventRepository.py
@@ -6,17 +6,22 @@ import json
 class eventRepository:
 
     def __init__(self):
-        self.client = MongoClient('mongodb', 27017)
+        self.client = MongoClient('127.0.0.1', 27017) # mongodb
         self.db = self.client.workday_db
         self.events = self.db.event_collection
 
-
+    """
+    get event by id
+    """
     def get_id(self, event_id):
         event = self.events.find_one({"_id": ObjectId(event_id)})
         event = json.loads(json_util.dumps(event))
         event["_id"] = event["_id"]["$oid"]
         return event
 
+    """
+    get all events per user
+    """
     def get_all(self):
         cursor = self.events.find({})
         events = list(cursor)
@@ -25,17 +30,34 @@ class eventRepository:
             item["_id"] = item["_id"]["$oid"]
         return events
 
+    """
+    save event
+    """
     def save(self, event):
         persisted_event = self.events.insert_one(event)
         new_id = json.loads(json_util.dumps(persisted_event.inserted_id))
         return list(new_id.values())[0]
 
+
+    """
+    update event
+    """
     def update(self, event):
         event_id = event["_id"]
         del event["_id"]
         result = self.events.update_one(filter={"_id": ObjectId(event_id)}, update={"$set": event})
         return result.modified_count
 
+    """
+    delete event
+    """
     def delete(self, event):
         result = self.events.delete_one({"_id": ObjectId(event)})
+        return result.deleted_count
+
+    """"
+    delete many events
+    """
+    def delete_many(self, query):
+        result = self.events.delete_many(query)
         return result.deleted_count
